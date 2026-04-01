9c1d04ab70849ac74cc5c79a4f2a6be6d483aa59
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-04-01 19:08:49 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Products from './components/Products'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+    <Navbar />
+    <Products />
+    </div>
   )
 }
 
